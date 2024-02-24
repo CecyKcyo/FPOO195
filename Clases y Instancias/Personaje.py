baee25ffa19fec492f78fc44a1e3c12e1813ccbc
CecyKcyo <122041716@upq.edu.mx>
2024-02-23 19:24:22 -0600

    Personaje.py

--- Clases y Instancias/Personaje.py
+++ Clases y Instancias/Personaje.py
@@ -11,3 +11,17 @@ class Personaje:
             
         else:
             print("El personaje"+self.nombre+"Esta muerto")
+             
+    def lanzarGranada(self):
+        print(self.nombre+" Pego una granada")
+        
+    def recargarGranada(self, municion):
+        cargador= 25
+        cargador=cargador+municion
+        print("Arma recargada al"+ str(cargador)+"%")
+     
+    #instancia creamos eo objeto de la clase personaje   
+spartan=Personaje()
+print(spartan.nombre)
+print(spartan.especie)
+print(spartan.recargarGranada)
