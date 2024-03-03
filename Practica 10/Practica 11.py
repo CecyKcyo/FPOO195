421c0e818de7f5f54f73e225df9c812fe202ee71
CecyKcyo <122041716@upq.edu.mx>
2024-03-03 15:13:17 -0600

    Update Practica 11.py

--- Practica 10/Practica 11.py
+++ Practica 10/Practica 11.py
@@ -3,7 +3,8 @@ from tkinter import Tk, Frame
 ventana=Tk() #Uso de poo creando objetos ventana 
 ventana.title("Ejemplo con 3 frames")
 ventana.geometry("600x400")
-
+ 
+ 
 #colocamos frames de la ventana
 seccion1=Frame(ventana, bg="pink")
 seccion1.pack(expand=True, fill='both')
