e1a16bc00b7bac9aeb08b44d586de418e784881f
CecyKcyo <122041716@upq.edu.mx>
2024-03-20 17:51:41 -0600

    .

--- TKINTHER/Practica 14 Bancooo/Cuenta.py
+++ TKINTHER/Practica 14 Bancooo/Cuenta.py
@@ -19,6 +19,21 @@ class Cuenta:
             self.__saldo -= cantidad
             return True
         return False
+    
+    def retirar_efectivo(self, cantidad):
+        """Intenta retirar una cantidad de dinero de la cuenta.
+        
+        Args:
+            cantidad (float): La cantidad de dinero a retirar.
+        
+        Returns:
+            bool: True si el retiro fue exitoso, False si no hay saldo suficiente.
+        """
+        if cantidad <= self.saldo:
+            self.saldo -= cantidad
+            return True
+        else:
+            return False
 
     @property
     def numero_cuenta(self):
